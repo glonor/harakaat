0ebbb93f422e0a5fe84575d807ca92d567f95986
Glonor <mouezkhelifi96@gmail.com>
2024-04-18 09:22:04 +0200

    Update training schedule in schedule.sh

--- scripts/schedule.sh
+++ scripts/schedule.sh
@@ -2,6 +2,9 @@
 # Schedule execution of many runs
 # Run from root folder with: bash scripts/schedule.sh
 
-python src/train.py trainer=ddp logger=wandb experiment=3000_steps.yaml model.max_length=128 data.max_len=128
-python src/train.py trainer=ddp logger=wandb experiment=3000_steps.yaml model.max_length=256 data.max_len=256
-python src/train.py trainer=ddp logger=wandb experiment=3000_steps.yaml model.max_length=512 data.max_len=512
+# python src/train.py trainer=ddp logger=wandb experiment=3000_steps.yaml model.max_length=128 data.max_len=128
+# python src/train.py trainer=ddp logger=wandb experiment=3000_steps.yaml model.max_length=256 data.max_len=256
+# python src/train.py trainer=ddp logger=wandb experiment=3000_steps.yaml model.max_length=512 data.max_len=512
+
+python src/train.py trainer=ddp logger=wandb experiment=3000_steps.yaml trainer.accumulate_grad_batches=256
+python src/train.py trainer=ddp logger=wandb experiment=3000_steps.yaml trainer.accumulate_grad_batches=64
